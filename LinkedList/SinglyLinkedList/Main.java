f6ae070c42b276eb94102354ea023877592b84dc
demo <manoharan12105@gmail.com>
2026-06-27 22:51:38 +0530

    first commit

---
+++ LinkedList/SinglyLinkedList/Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList sl = new SinglyLinkedList();
+
+        // Insert first
+        sl.insertFirst(3);
+        sl.insertFirst(13);
+        sl.insertFirst(23);
+        sl.insertFirst(34);
+        sl.insertFirst(2);
+
+        // Insert Last
+        sl.insertLast(99);
+        sl.insertLast(67);
+
+        // Insert at any index(0 based) (index, data)
+        sl.insert(6, 47);
+        sl.insert(0, 0);
+        sl.insert(7654, 200);
+        sl.insert(5, 78);
+        
+        // Display
+        sl.display();
+
+        // Delete First
+        sl.deleteFirst();
+        sl.display();
+
+        // Delete Last
+        sl.deleteLast();
+        sl.display();
+    }
+}
---
+++ LinkedList/SinglyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,141 @@
+public class SinglyLinkedList {
+  private Node head;
+  private Node tail;
+
+  private int size;
+
+  public SinglyLinkedList() {
+    this.size = 0;
+  }
+
+
+  /**
+   * Node Class
+   */
+  private class Node{
+    private int data;
+    private Node next;
+
+    public Node(int data) {
+      this.data = data;
+    }
+
+    public Node(int data, Node next) {
+      this.data = data;
+      this.next = next;
+    }
+  }
+
+
+
+
+  // Insertion - First (1)
+  public void insertFirst(int data) {
+    Node node = new Node(data);
+    node.next = head;
+    head = node;
+
+    if(tail == null) {
+      tail = head;
+    }
+
+    size++;
+  }
+
+
+
+  // Insertion - Last (3)
+  public void insertLast(int data) {
+    if(tail == null) {
+      insertFirst(data);
+      return;
+    }
+
+    Node node = new Node(data);
+    tail.next = node;
+    tail = node;
+    size++;
+  }
+
+
+  // Insertion at any index(0 Based index) (4)
+  public void insert(int index, int data) {
+    if(index == 0) {
+      insertFirst(data);
+      return;
+    } else if (index >= size) {
+      insertLast(data);
+      return;
+    }
+
+    Node node = new Node(data);
+
+    Node temp = head;
+
+    for (int i = 1; i < index; i++) {
+      temp = temp.next;
+    }
+
+    node.next = temp.next;
+    temp.next = node;
+    size++;
+
+  }
+
+
+
+  // Deletion - first (5)
+  public void deleteFirst() {
+    head = head.next;
+    if(head == null) {
+      tail = null;
+    }
+
+    size--;
+  }
+
+
+
+  // Deletion - last (6)
+  public void deleteLast() {
+    if(size <= 1) {
+      deleteFirst();
+      return;
+    }
+
+    // Node temp = head;
+
+    // while(temp.next != tail) {
+    //   temp = temp.next;
+    // }
+
+    Node temp = get(size - 2);
+
+    temp.next = null;
+    tail = temp;
+    size--;
+  }
+
+
+
+  // Display LinkedList (2)
+  public void display() {
+    Node temp = head;
+
+    while(temp != null) {
+      System.out.print(temp.data + " -> ");
+      temp = temp.next;
+    }
+
+    System.out.println("End");
+  }
+
+  public Node get(int index) {
+    Node node = head;
+    for (int i = 0; i < index; i++) {
+      node = node.next;
+    }
+
+    return node;
+  }
+}
