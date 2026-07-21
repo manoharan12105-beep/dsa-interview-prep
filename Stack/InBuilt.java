ea374d70988e1957d0b8b80861dce5de59b3f343
Manoharan M <manoharan12105@gmail.com>
2026-07-21 21:49:50 +0530

    Update InBuilt.java

--- Stack/InBuilt.java
+++ Stack/InBuilt.java
@@ -11,16 +11,16 @@ public class InBuilt {
     stack.add(34);
     stack.addAll(2, Arrays.asList(1, 2, 3));
 
-    System.out.println("Stack : " + stack);                          // [12, 34, 1, 2, 3]
-    System.out.println("Pop   : " + stack.pop());                    // 3
-    System.out.println("Stack : " + stack);
-    System.out.println("Peek Element :" + stack.peek());                   // 2
-    System.out.println("Is Empty ? " + stack.isEmpty());                // false
-    System.out.println("Capacity  : " +stack.capacity());               // 10
-    System.out.println("Contains 34 ? " + stack.contains(34));           // true
-    System.out.println("Index of 12 : " + stack.indexOf(12));            // 0
-    System.out.println("Index of 1 from index 2 : " + stack.indexOf(1, 2));       // 0
-    System.out.println("Element At : " + stack.elementAt(3));      // 2
-    System.out.println("Size of stack : " + stack.size());                   //
+    System.out.println("Stack : " + stack);                                          // [12, 34, 1, 2, 3]
+    System.out.println("Pop   : " + stack.pop());                                    // 3
+    System.out.println("Stack : " + stack);                                          // [12, 34, 1, 2]
+    System.out.println("Peek Element :" + stack.peek());                             // 2
+    System.out.println("Is Empty ? " + stack.isEmpty());                             // false
+    System.out.println("Capacity  : " +stack.capacity());                            // 10
+    System.out.println("Contains 34 ? " + stack.contains(34));                       // true
+    System.out.println("Index of 12 : " + stack.indexOf(12));                        // 0
+    System.out.println("Index of 1 from index 2 : " + stack.indexOf(1, 2));          // 0
+    System.out.println("Element At : " + stack.elementAt(3));                        // 2
+    System.out.println("Size of stack : " + stack.size());                           // 4
   }
 }
